0d2c6fd191dee7fede2154642d5422a986c37f8f
Krasimir Berov <k.berov@gmail.com>
2012-08-19 11:19:49 +0300

    example application and runner script - initial commit

---
+++ examples/lib/My/App.pm
@@ -0,0 +1,119 @@
+package My::App;
+use strict;
+use warnings;
+use utf8;
+use Data::Dumper;
+use CGI qw(:standard :html3);
+use DBD::SQLite;
+use My;
+
+sub new {
+  my $class = shift;
+  my $self = bless {}, $class;
+  return $self;
+}
+
+sub run {
+  my $class = shift;
+
+  #initialise
+  my $app = $class->new();
+  $app->initialise_db;
+  my $action = 'action_' . (param('do') || 'list_users');
+  warn $action . $app->can($action);
+
+  #run
+  if ($app->can($action)) {
+    print header(
+      -type    => 'text/html',
+      -status  => '200 OK',
+      -charset => 'utf-8',
+      ),
+      start_html($action), $app->$action(), end_html();
+  }
+  else {
+    print header(
+      -type    => 'text/html',
+      -status  => '404 Not Found',
+      -charset => 'utf-8',
+      ),
+      start_html(-title => 'Page Not Found'), h1('Page Not Found'), end_html();
+  }
+}
+
+
+sub initialise_db {
+  my $app  = shift;
+  DBIx::Simple::Class->DEBUG(1);
+
+  my $dbix = DBIx::Simple->connect(
+    "dbi:SQLite:dbname=$ENV{users_HOME}/db.sqlite",
+    {sqlite_unicode => 1, RaiseError => 1}
+  ) || die $DBI::errstr;
+  DBIx::Simple::Class->dbix($dbix);
+  $dbix->query(<<"TAB");
+        CREATE TABLE IF NOT EXISTS groups(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          group_name VARCHAR(12),
+          "foo-bar" VARCHAR(13),
+          data TEXT
+          )
+TAB
+  $dbix->query(<<"TAB");
+        CREATE TABLE IF NOT EXISTS users(
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          group_id INT default 1,
+          login_name VARCHAR(12),
+          login_password VARCHAR(100), 
+          disabled INT DEFAULT 1
+          )
+TAB
+
+  sub My::Group::ALIASES {
+    {data => 'group_data', 'foo-bar' => 'foo_bar'};
+  }
+  My::Group->QUOTE_IDENTIFIERS(1);
+  My::User->QUOTE_IDENTIFIERS(1);
+
+}
+
+sub action_list_users {
+  my $app = shift;
+  my $out = '';
+
+  return $out;
+}
+
+sub action_add_user {
+  my $app = shift;
+  my $out = '';
+
+  return $out;
+}
+
+sub action_list_groups {
+  my $app    = shift;
+  my $G      = 'My::Group';
+  my @groups = $G->query($G->SQL('SELECT'));
+  my $out    = '';
+
+  if (@groups) {
+
+  }
+  else {
+    $out .= h1('Please addd a group'), a({-href => 'action=add_group'}, 'Add');
+
+  }
+
+  return $out;
+}
+
+sub action_add_group {
+  my $app = shift;
+  my $out = '';
+
+  return $out;
+}
+
+
+1;
--- examples/users.cgi
+++ examples/users.cgi
@@ -1,11 +1,16 @@
-#!/usr/env perl
-# Example CGI script.
+#!/usr/bin/env perl
+# Example minimal and naive CGI application.
 #
 use 5.10.1;
 use strict;
 use warnings;
 use utf8;
+BEGIN {
+  use File::Basename 'dirname';
+  use Cwd;
+  $ENV{users_HOME} = Cwd::abs_path(dirname(__FILE__));
+}
+use lib ($ENV{users_HOME}.'/lib');
+use My::App;
 
-use File::Basename 'dirname';
-use Cwd;
-use lib (Cwd::abs_path(dirname(__FILE__)).'/lib');
+My::App->run();
