95813d75887589b6bf703d9ad9d79b70de2c5d79
Krasimir Berov <k.berov@gmail.com>
2012-08-31 02:23:25 +0300

    Pseudo code for loading schema

--- lib/DBIx/Simple/Class/Schema.pm
+++ lib/DBIx/Simple/Class/Schema.pm
@@ -6,25 +6,91 @@ use Carp;
 use parent 'DBIx::Simple::Class';
 *_get_obj_args = \&DBIx::Simple::Class::_get_obj_args;
 
-sub load_schema {
+sub _get_table_info {
   my ($class, $args) = _get_obj_args(@_);
 
+  #get tables from the current database
   #see https://metacpan.org/module/DBI#table_info
-  my $tables = $class->dbh->table_info(
+  return $class->dbh->table_info(
     undef, undef,
     $args->{table} || '%',
     $args->{type}  || "'TABLE','VIEW'"
   )->fetchall_arrayref({});
 
-  #return $tables;
-  my $tables_columns = {};
+}
+
+sub _get_column_info {
+  my ($class, $tables) = @_;
+  my $tables_with_columns = {};
   foreach my $table (@$tables) {
-    $tables_columns->{$table->{TABLE_NAME}} =
+    $tables_with_columns->{$table->{TABLE_NAME}}{column_info} =
       $class->dbh->column_info(undef, undef, $table->{TABLE_NAME}, '%')
       ->fetchall_arrayref({});
   }
-  return $tables_columns;
+  return $tables_with_columns;
 }
+
+sub _generate_COLUMNS {
+  my ($class, $tables) = @_;
+
+  #COLUMN_NAME: The column identifier.
+}
+
+sub _generate_ALIASES {
+  my ($class, $tables) = @_;
+
+  #COLUMN_NAME: The column identifier.
+}
+
+sub _generate_CHECKS {
+  my ($class, $tables) = @_;
+  #
+
+}
+
+sub _generate_PRIMARY_KEY {
+  my ($class, $tables) = @_;
+  foreach my $table ($tables) {
+    $class->dbh->primary_key_info(undef, undef, $table->{TABLE_NAME});
+  }
+  return $tables;
+}
+
+sub _generate_CODE {
+  my ($class, $tables) = @_;
+  my $code = '';
+
+  return $code;
+}
+
+sub load_schema {
+  my ($class, $args) = _get_obj_args(@_);
+  $args->{namespace}
+    || Carp::croak(
+    'Please provide "namespace"' . ' (e.g. My::Model) for your classes. ');
+
+  my $table_info = $class->_get_table_info($args);
+
+  #return $tables;
+  #get table columns
+  $table_info = $class->_get_column_info($table_info);
+
+  #generate COLUMNS
+  $table_info = $class->_generate_COLUMNS($table_info);
+
+  #generate ALIASES
+  $table_info = $class->_generate_ALIASES($table_info);
+
+  #generate PRIMARY_KEY
+  $table_info = $class->_generate_PRIMARY_KEY($table_info);
+
+  #generate CHECKS
+  $table_info = $class->_generate_CHECKS($table_info);
+
+  #generate code
+  return $class->_generate_CODE($table_info);
+}
+
 
 sub dump_schema_at {
 
@@ -44,6 +110,28 @@ __END__
 
 DBIx::Simple::Class::Schema - Create and use a DBIx::Simple::Class schema from a database
 
+=head1 SYNOPSIS
+
+  #Somewhere in a utility script or startup() fo your application.
+  DBIx::Simple::Class::Schema->dbix(DBIx::Simple->connect(...));
+  my $perl_code = DBIx::Simple::Class::Schema->load_schema(
+    namespace =>'My::Model',
+    table => '%',#all tables from the current database
+    type  => "'TABLE','VIEW'"# make classes for tables and views
+  );
+
+  #Now eval() to use your classes.
+  eval $perl_code|| croak($@);
+
+  #Or save it for more customisations and later usage.
+  DBIx::Simple::Class::Schema->dump_schema_at(
+    code => $perl_code,
+    root => "$ENV{HOME}/$ENV{PERL_LOCAL_LIB_ROOT}/lib"
+    overwrite =>1 #overwrite existing files
+  );
+
+
+=head1 DESCRIPTION
 
 
 =head1 LICENSE AND COPYRIGHT
